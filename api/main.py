2ba49428bfceade18fee914c5ab3ec4d9e116f78
MUZAMIL ALI <muzamil.workdev@gmail.com>
2025-11-23 17:51:38 +0500

    added

--- api/main.py
+++ api/main.py
@@ -5,17 +5,21 @@ import requests
 
 app = FastAPI()
 
-# ADD THIS CORS BLOCK — allows ALL origins (perfect for portfolio)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],              # Change to your actual domain later if you want
+    allow_origins=["https://portfolio-tan-two-72.vercel.app"],              # Change to your actual domain later if you want
     allow_credentials=True,
     allow_methods=["*"],              # GET, POST, etc.
     allow_headers=["*"],              # Allows Authorization, Content-Type, etc.
 )
 
+import os
+API_KEY = os.getenv("API_KEY")
+
 COUNTER_URL = "https://api.counterapi.dev/v2/muzamil-sulemans-team-1761/portfolio-views-1023"
-API_KEY = "ut_jht5rqbJ9MVvmo3ygJa6lnk1jq3n6ny762d42nAs"
+
+
 headers = {"Authorization": f"Bearer {API_KEY}"}
 
 @app.get("/get_views")
